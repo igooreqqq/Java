75c9ae587e1044ea519bfb0b260e2b6316b21f04
igooreqqq <109036041+igooreqqq@users.noreply.github.com>
2023-04-23 19:17:25 +0200

    Create Main.java

---
+++ hackerrank/Sales by Match/Main.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+    int numOfPairs = 0;
+    List<Integer> processed = new ArrayList<Integer>();
+
+    for (int i = 0; i < n; i++) {
+        int current = ar.get(i);
+        if (!processed.contains(current)) {
+            int count = Collections.frequency(ar, current);
+            int pairs = count / 2;
+            numOfPairs += pairs;
+            processed.add(current);
+        }
+    }
+
+    return numOfPairs;
+}
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
